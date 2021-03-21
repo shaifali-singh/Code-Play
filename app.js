6701722aac4857dc82d7c0c5223f78067da21299
Shaifali Singh <shaifalisingh916@gmail.com>
2021-03-22 02:57:38 +0530

    Login and sigup page added

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
-var path    = require('path');
-var express = require('express');
-var app     = express();
+var path           = require('path');
+var express        = require('express');
+var app            = express();
+var methodOverride = require('method-override');
 
+app.use(methodOverride('_method'));
+//for parsing 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/",function(req,res){
-    res.sendFile('landing.html', {root : __dirname + '/views'});
-})
+    // res.sendFile('landing.html', {root : __dirname + '/views'});
+    res.render("landing");
+});
 
+app.get("/login", (req,res) =>{
+   res.render("login");
+});
+
+app.get("/signup", (req,res) => {
+    res.render("signup");
+});
+
+app.post("/login", (req,res) => {
+
+});
+
+app.post("/signup", (req,res) => {
+    
+});
 
 const PORT = process.env.PORT || 3000;
 
